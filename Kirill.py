657f01816fbafa7d204827f3c3e3f3f1dcafca72
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-10-29 18:37:13 +0300

    Update Kirill.py

--- Kirill.py
+++ Kirill.py
@@ -1,1 +1,12 @@
-
+'''это код абсолютно бесполезен'''
+class KirillCode:
+    ''' Просто класс'''
+    def __init__(self):
+        '''Просто инит'''
+        self.frase = 'Я Кирилл'
+    def write(self):
+        '''Если Вы знаете питон то не читайте этот код'''
+        return self.frase
+    def new_text(self, text):
+        '''ПРосто не читайте'''
+        self.frase = text
